ae5547ad48b712126592691902f515226cadf0e9
AnSantos99 <44712120+AnSantos99@users.noreply.github.com>
2019-05-31 10:23:21 +0100

    Sorting players scores

---
+++ PlayerManager3/Player.cs
@@ -0,0 +1,39 @@
+using System;
+
+namespace Exercicio2
+{
+    /// <summary>
+    /// A player.
+    /// </summary>
+    public class Player : IComparable<Player>
+    {
+        /// <summary>
+        /// The name of the player.
+        /// </summary>
+        /// <value>A string representing the name of the player.</value>
+        public string Name { get; }
+
+        /// <summary>
+        /// The score.
+        /// </summary>
+        /// <value>An integer representing the score of the player.</value>
+        public int Score { get; set; }
+
+        /// <summary>
+        /// Creates a new player.
+        /// </summary>
+        /// <param name="name">The player name.</param>
+        /// <param name="score">The initial player score.</param>
+        public Player(string name, int score)
+        {
+            Name = name;
+            Score = score;
+        }
+
+        public int CompareTo(Player other)
+        {
+            if(other == null) return -1;
+            return other.Score - this.Score;
+        }
+    }
+}
