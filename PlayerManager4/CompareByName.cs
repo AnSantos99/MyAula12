20d385720d2ee2e929f8dca7d979cf96fefe5c1a
AnSantos99 <44712120+AnSantos99@users.noreply.github.com>
2019-05-31 11:00:32 +0100

    Added ComparerByName class

---
+++ PlayerManager4/CompareByName.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayerManager4
+{
+    public class CompareByName : IComparer<Player>
+    {
+        private bool ord;
+
+        public CompareByName(bool ord)
+        {
+            this.ord = ord;
+        }
+        public int Compare(Player x, Player y)
+        {
+            if (x == y) return 0;
+            if (x == null) return 1;
+            if (y == null) return -1;
+            if (ord) return x.Name.CompareTo(y.Name);
+            return y.Name.CompareTo(x.Name);
+            
+        }
+    }
+}
